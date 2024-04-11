b07fbb5922b1ebfb58f92603a5aa3b1418115567
isOdin <ivanbildagin@mail.ru>
2024-04-11 19:27:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import TelegramBot from "node-telegram-bot-api";
+import 'dotenv/config';
+
+const token = process.env.token;
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('text', async (msg) =>{
+    try {
+        if(msg.from.username === 'pipisabot' && msg.from.is_bot && !msg.text.includes('Следующая попытка завтра!')){
+            await bot.deleteMessage(msg.chat.id, msg.from.id);
+        }
+    } catch (error) {
+        console.log(error);
+        console.log(msg)
+    }
+});
